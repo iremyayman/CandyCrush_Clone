bcec8a0e58ba42c492518ea0126769a1d58d2d29
Irem <iremyayman12@gmail.com>
2021-09-01 17:45:42 +0300

    Adding Checking for Fives and View

--- app.js
+++ app.js
@@ -104,44 +104,42 @@ document.addEventListener('DOMContentLoaded',() =>{
     //Checking for matches
     //Check for row of three
 
-    function checkRowForThree(){
-        for(i = 0; i<61 ; i++){
-            let rowOfThree= [i,i+1,i+2]
+
+    function checkRowForFive(){
+        for(i=0;i<59;i++){
+            let rowOfFive=[i,i+1,i+2,i+3,i+4]
             let decidedColor=squares[i].style.backgroundImage
-            const isBlank = squares[i].style.backgroundImage===''
-            const notValid=[6,7,14,15,22,23,30,31,38,39,46,47,54,55]
-            if(notValid.includes(i)) continue
-            if (rowOfThree.every(index=> squares[index].style.backgroundImage===decidedColor&&!isBlank)){
-               
-                score+=3
+            const isBlank=squares[i].style.backgroundImage===''
+            const notValid=[4,5,6,7,12,13,14,15,20,21,22,23,28,29,30,31,36,37,38,39,44,45,46,47,52,53,54,55]
+            if(notValid.includes(i))continue
+            if(rowOfFive.every(index=>squares[index].style.backgroundImage===decidedColor&&!isBlank)){
+                score+=5
                 scoreDisplay.innerHTML=score
-                rowOfThree.forEach(index=>{
-                    squares[index].style.backgroundImage=''
+                rowOfFive.forEach(index=>{
+                   squares[index].style.backgroundImage=''
                 })
             }
 
         }
     }
-    checkRowForThree()
-    function checkColumnForThree(){
-        for(i = 0; i<47 ; i++){
-            let columnOfThree= [i,i+width,i+width*2]
+
+
+    function checkColumnForFive(){
+        for (i=0 ; i<23 ;i++){
+            let columnOfFive=[i,i+width,i+width*2,i+width*3,i+width*4]
             let decidedColor=squares[i].style.backgroundImage
-            const isBlank = squares[i].style.backgroundImage===''
-            if (columnOfThree.every(index=> squares[index].style.backgroundImage===decidedColor&&!isBlank)){
-               
-                score+=3
+            const isBlank=squares[i].style.backgroundImage===''
+            if(columnOfFive.every(index=>squares[index].style.backgroundImage===decidedColor&&!isBlank )){
+                score+=5
                 scoreDisplay.innerHTML=score
-                columnOfThree.forEach(index=>{
+                columnOfFive.forEach(index=>{
                     squares[index].style.backgroundImage=''
                 })
             }
 
         }
-    }
-    checkColumnForThree()
 
-    
+    }
     function checkRowForFour(){
         for(i = 0; i<60 ; i++){
             let rowOfFour= [i,i+1,i+2,i+3]
@@ -161,8 +159,9 @@ document.addEventListener('DOMContentLoaded',() =>{
         }
     }
     checkRowForFour()
+
     function checkColumnForFour(){
-        for(i = 0; i<47 ; i++){
+        for(i = 0; i<39 ; i++){
             let columnOfFour= [i,i+width,i+width*2,i+width*3]
             let decidedColor=squares[i].style.backgroundImage
             const isBlank = squares[i].style.backgroundImage===''
@@ -178,8 +177,55 @@ document.addEventListener('DOMContentLoaded',() =>{
         }
     }
     checkColumnForFour()
-    window.setInterval(function(){
+  
+    
+    
+
+    function checkRowForThree(){
+        for(i = 0; i<61 ; i++){
+            let rowOfThree= [i,i+1,i+2]
+            let decidedColor=squares[i].style.backgroundImage
+            const isBlank = squares[i].style.backgroundImage===''
+            const notValid=[6,7,14,15,22,23,30,31,38,39,46,47,54,55]
+            if(notValid.includes(i)) continue
+            if (rowOfThree.every(index=> squares[index].style.backgroundImage===decidedColor&&!isBlank)){
+               
+                score+=3
+                scoreDisplay.innerHTML=score
+                rowOfThree.forEach(index=>{
+                    squares[index].style.backgroundImage=''
+                })
+            }
+
+        }
+    }
+    checkRowForThree()
+
+
+    function checkColumnForThree(){
+        for(i = 0; i<47 ; i++){
+            let columnOfThree= [i,i+width,i+width*2]
+            let decidedColor=squares[i].style.backgroundImage
+            const isBlank = squares[i].style.backgroundImage===''
+            if (columnOfThree.every(index=> squares[index].style.backgroundImage===decidedColor&&!isBlank)){
+               
+                score+=3
+                scoreDisplay.innerHTML=score
+                columnOfThree.forEach(index=>{
+                    squares[index].style.backgroundImage=''
+                })
+            }
+
+        }
+    }
+    checkColumnForThree()
+
+    
+   window.setInterval(function(){
         moveDown()
+        checkColumnForFive()
+        checkRowForFive()
+        
         checkColumnForFour()
         checkRowForFour()
 
